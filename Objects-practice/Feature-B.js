cca1a90421312ec5b9bbb6f1e13814a4963d76ad
Md Ehtesham <mdehtesham313@gmail.com>
2025-02-09 17:47:53 +0530

    Laptop object is created

--- Objects-practice/Feature-B.js
+++ Objects-practice/Feature-B.js
@@ -1,1 +1,10 @@
-console.log("New branch changes added");
+const laptop = {
+  brand: "HP",
+  series: "Gaming",
+  processor: "i7",
+  price: 56000,
+  gpu: "GTX1650",
+  battery: "Li-Po",
+  origin: "China"
+}
+
